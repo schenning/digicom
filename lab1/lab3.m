05aa58faad76e472d8a2eef1ca93608b7caa17cb
schenning <henning.schei@gmail.com>
2016-01-20 13:44:07 +0100

    g

--- lab1/lab3.m
+++ lab1/lab3.m
@@ -5,7 +5,6 @@ close all
 N_RB_DL = 50;  % Number of Recource blocks
 
 lte_gt = lte_rs_gold(0,N_idcell);
-
 l=0;
 p_l_Ns = zeros(2,100);
 p_l_Ns(1,:) = lte_rs(0,N_idcell,l,0,N_RB_DL,lte_gt);
@@ -13,12 +12,10 @@ l=1;
 p_l_Ns(2,:) = lte_rs(0,N_idcell,l,0,N_RB_DL,lte_gt);
 
 
-
-
 %v_shift = cellID mod 6
 
 startpoint_sss= N_f-72-1024-1023;
-endpoint_sss= N_f-72-1024;
+endpoint_sss= N_f-72-1024; % Using this to find time instance of first PBCH-symbol
 
 
 %Exetract the PBCH
@@ -43,8 +40,6 @@ for i=1:5
     R_e(:,i) = [R(1024+(-299:0),i) ; R(2:301,i)]; %
 end
 
-
-% Symbol 7
     
 v=mod(N_idcell,6) +1; 
 
@@ -54,40 +49,36 @@ RS70 = R_e(v:6:end,1); % for p=0
 RS71 = R_e(mod(v+3,6):6:end,1); % for p=1
 RS110= R_e(mod(v+3,6):6:end,5); % p=0
 RS111= R_e(v:6:end,5); % p=1
-idxPRB22 = 12*22+1;
+
+idxPRB22 = 12*22+1; % startindex of PRB22
 
 % Exetracting resource elements and set pilots to zero
-RE7 = R_e(idxPRB22:12*28,1);
-RE7(min(v,mod(v+3,6)):3:end)=0; 
-RE8 = R_e(idxPRB22:12*28,2);
-RE8(min(v,mod(v+3,6)):3:end) = 0;
-RE9 = R_e(idxPRB22:12*28,3);
-RE10 = R_e(idxPRB22:12*28,4);
+RE7 = R_e( idxPRB22: 12*28, 1);
+RE7(min( v, mod(v+3,6)):3:end) = 0; 
+RE8 = R_e( idxPRB22: 12*28, 2);
+RE8(min(v, mod(v+3,6)): 3: end) = 0;
+RE9 = R_e( idxPRB22:12*28,3);
+RE10 = R_e( idxPRB22:12*28,4);
 k=1;
-for i=1:72
-    if RE7(i)~= 0
-        seven(k)=RE7(i);
-        k=k+1;
+
+% Removing the indices in RE7 and RE8 that contain pilots 
+% in order to extract the PRB's in RE7 and RE8
+for i=1:48
+    if RE7(i) == 0
+        RE7(i)= [];
     end
 end
-k=1;
-for i=1:72
-    if RE8(i) ~= 0
-        eight(k)= RE8(i);
-        k=k+1;
+
+for i=1:48
+    if RE8(i) == 0
+        RE8(i)= [];
     end
 end
 
 
 % Resource elments
 
-RE = [RE10 ;RE9 ;eight' ;seven'];
-
-
-
-
-
-
+RE = [RE10 ;RE9 ;RE8 ; RE7];
 
 
 % channel estimation
@@ -117,14 +108,9 @@ plot(20*log10(abs(H1_11_est)),'b')
 
 
 
-R_ext2 = R_e(12*21-1:12*27);
 
 
 
-
-
-
-%RS07= 
 
 
 
